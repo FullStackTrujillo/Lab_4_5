9c4852c1779885eecf3116fe5cc4c4806b8c9a75
Jair Trujillo <jtrujillo2@my.ancoraed.com>
2020-07-10 10:14:24 -0700

    Add project files.

---
+++ Lab_4_5/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Step 1");
+            List<int> scores = new List<int>();
+            scores.Add(4400);
+            scores.Add(8900);
+            scores.Add(1200);
+            scores.Add(3700);
+            scores.Add(7200);
+
+            foreach (int s in scores)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("Step 2");
+
+            scores.Sort();
+
+            foreach (int s in scores)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("Step 3");
+
+            scores.Reverse();
+
+            foreach (int s in scores)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("Step 4");
+
+            scores.Add(2400);
+
+            foreach (int s in scores)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("Step 5");
+
+            scores.Remove(4400);
+
+            foreach (int s in scores)
+            {
+                Console.WriteLine(s);
+            }
+ 
+
+
+
+
+
+        }
+    }
+}
